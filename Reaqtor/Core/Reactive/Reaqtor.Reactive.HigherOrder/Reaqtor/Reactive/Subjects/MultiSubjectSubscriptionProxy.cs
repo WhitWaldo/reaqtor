b4dcb44a3f4640b4c6aa07311d6a97565e36f138
Whit Waldo <whit.waldo@innovian.net>
2023-01-26 08:31:40 -0600

    Implemented IDisposable for subscription, but since this was done in OnDisposing, the compiler isn't aware of it, so applied pragma statement to dismiss the warning

--- Reaqtor/Core/Reactive/Reaqtor.Reactive.HigherOrder/Reaqtor/Reactive/Subjects/MultiSubjectSubscriptionProxy.cs
+++ Reaqtor/Core/Reactive/Reaqtor.Reactive.HigherOrder/Reaqtor/Reactive/Subjects/MultiSubjectSubscriptionProxy.cs
@@ -10,7 +10,9 @@ namespace Reaqtor.Reactive
 {
     internal sealed class MultiSubjectSubscriptionProxy<TInput, TOutput> : UnaryOperator<Uri, TOutput>
     {
+#pragma warning disable CA2213
         private readonly SingleAssignmentSubscription _subscription = new();
+#pragma warning restore CA2213
 
         public MultiSubjectSubscriptionProxy(Uri uri, IObserver<TOutput> observer)
             : base(uri, observer)
@@ -31,6 +33,17 @@ namespace Reaqtor.Reactive
             SubscriptionInitializeVisitor.Subscribe(sub);
 
             _subscription.Subscription = sub;
+        }
+
+        /// <summary>
+        /// Defines behavior immediately after we `Dispose` of the operator. This method is
+        /// usually called at the very end of the `Dispose` method.
+        /// </summary>
+        protected override void OnDispose()
+        {
+            _subscription?.Dispose();
+
+            base.OnDispose();
         }
     }
 }
